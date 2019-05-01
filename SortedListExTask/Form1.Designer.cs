d850993851da8a94c4133b8a4987878b5ed6536d
Tehqq <th90@ymail.com>
2019-05-01 09:35:32 -0300

    Final commit

--- SortedListExTask/Form1.Designer.cs
+++ SortedListExTask/Form1.Designer.cs
@@ -56,6 +56,7 @@
             this.btnRemoveTask.TabIndex = 15;
             this.btnRemoveTask.Text = "Remove Task";
             this.btnRemoveTask.UseVisualStyleBackColor = true;
+            this.btnRemoveTask.Click += new System.EventHandler(this.btnRemoveTask_Click);
             // 
             // lstTasks
             // 
@@ -64,6 +65,7 @@
             this.lstTasks.Name = "lstTasks";
             this.lstTasks.Size = new System.Drawing.Size(188, 95);
             this.lstTasks.TabIndex = 14;
+            this.lstTasks.SelectedIndexChanged += new System.EventHandler(this.lstTasks_SelectedIndexChanged);
             // 
             // btnPrintAll
             // 
@@ -73,6 +75,7 @@
             this.btnPrintAll.TabIndex = 13;
             this.btnPrintAll.Text = "Print All Tasks";
             this.btnPrintAll.UseVisualStyleBackColor = true;
+            this.btnPrintAll.Click += new System.EventHandler(this.btnPrintAll_Click);
             // 
             // lblTaskDetails
             // 
@@ -90,6 +93,7 @@
             this.btnAddTask.TabIndex = 11;
             this.btnAddTask.Text = "Add Task";
             this.btnAddTask.UseVisualStyleBackColor = true;
+            this.btnAddTask.Click += new System.EventHandler(this.btnAddTask_Click);
             // 
             // txtTask
             // 
--- SortedListExTask/Form1.cs
+++ SortedListExTask/Form1.cs
@@ -12,9 +12,60 @@ namespace SortedListExTask
 {
     public partial class Form1 : Form
     {
+        private Dictionary<DateTime, string> taskList = new Dictionary<DateTime, string>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+        private void btnAddTask_Click(object sender, EventArgs e)
+        {
+            if (txtTask.Text != null || txtTask.Text != string.Empty)
+            {
+				if (!taskList.ContainsKey(dtpTaskDate.Value))
+				{
+					taskList.Add(dtpTaskDate.Value, txtTask.Text);
+					lstTasks.Items.Add(dtpTaskDate.Value);
+									
+					txtTask.Clear();
+					dtpTaskDate.Value = DateTime.Now;
+				}
+				else
+					MessageBox.Show("Only one task per date is allowed.");
+            }
+            else
+                MessageBox.Show("You must enter a task.");
+        }
+        private void lstTasks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex != -1)
+                lblTaskDetails.Text = taskList[(DateTime)lstTasks.SelectedItem].ToString();
+            else
+                lblTaskDetails.Text = string.Empty;
+        }
+        private void btnRemoveTask_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex != -1)
+            {
+                lstTasks.Items.Remove(lstTasks.SelectedItem);
+                taskList.Remove(Convert.ToDateTime(lstTasks.SelectedItem));
+            }
+            else
+                MessageBox.Show("Please select an item to remove.");
+        }
+        private void btnPrintAll_Click(object sender, EventArgs e)
+        {
+            if (taskList.Count == 0)
+                MessageBox.Show("There are no tasks");
+            else
+            {
+                string display = string.Empty;
+
+                foreach (KeyValuePair<DateTime, string> kvp in taskList)
+                    display += $"{kvp.Key.ToShortDateString()} {kvp.Value.ToString()} \n";
+
+                MessageBox.Show(display);
+            }
         }
     }
 }
